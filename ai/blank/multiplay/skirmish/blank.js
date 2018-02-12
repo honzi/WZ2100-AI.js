30112c77b1c8a61f03df7f20d3b2709184af7862
Jan Hořava <czechinthemail@gmail.com>
2018-02-12 12:14:53 -0800

    added more events

--- ai/blank/multiplay/skirmish/blank.js
+++ ai/blank/multiplay/skirmish/blank.js
@@ -1,2 +1,77 @@
+function eventAttacked(victim, attacker){
+}
+
+function eventAttackedUnthrottled(victim, attacker){
+}
+
+function eventBeacon(x, y, from, to){
+}
+
+function eventBeaconRemoved(from, to){
+}
+
+function eventChat(from, to, message){
+}
+
+function eventCheatMode(cheat){
+}
+
+function eventDestroyed(object){
+}
+
+function eventDroidBuilt(droid, structure){
+}
+
+function eventDroidIdle(droid){
+}
+
+function eventGameInit(){
+}
+
+function eventGameLoaded(){
+}
+
+function eventGameSaved(){
+}
+
+function eventGameSaving(){
+}
+
+function eventGroupLoss(object, group, size){
+}
+
+function eventLaunchTransporter(){
+}
+
+function eventMissionTimeout(){
+}
+
+function eventObjectSeen(sensor, object){
+}
+
+function eventObjectTransfer(object, from){
+}
+
+function eventPickup(){
+}
+
+function eventReinforcementsArrived(){
+}
+
+function eventResearched(research, structure){
+}
+
+function eventSelectionChange(selected){
+}
+
 function eventStartLevel(){
+}
+
+function eventStructureBuilt(structure){
+}
+
+function eventStructureReady(structure){
+}
+
+function eventVideoDone(){
 }
